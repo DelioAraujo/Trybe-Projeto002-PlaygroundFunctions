c829bfa9db61a735fbdc6596bac167b86b81f515
DelioAraujo <delioaraujo@gmail.com>
2023-01-30 18:52:38 -0300

    desafio 09 - tentativa 02

--- src/challenges.js
+++ src/challenges.js
@@ -100,49 +100,30 @@ const fizzBuzz = (array) =>{
 
 // Desafio 9 - Crie a função encode e a função decode
 
-const encode = (string) => {
+const encode = (string) =>{
 
-  let stringModificada;
-
-for(letra of string){
-      if (letra==="a"){
-          stringModificada= string.replaceAll(letra, "1")
-      } else if(letra==="e"){
-          stringModificada= string.replaceAll(letra, "2")
-      } else if(letra==="i"){
-          stringModificada= string.replaceAll(letra, "3")
-      } else if(letra==="o"){
-          stringModificada= string.replaceAll(letra, "4")
-      } else if(letra==="u"){
-          stringModificada= string.replaceAll(letra, "5")
-      }
-  }
-
-  return stringModificada
-
+let letraA= string.replaceAll("a", "1");
+let letraE= letraA.replaceAll("e", "2");
+let letraI= letraE.replaceAll("i", "3");
+let letraO= letraI.replaceAll("o", "4");
+let letraU= letraO.replaceAll("u", "5");
+    
+    
+return letraU
+    
 }
-
-
-const decode = (string) => {
-
-  let stringOriginal;
-
-for(letra of string){
-      if (letra==="1"){
-          stringOriginal= string.replaceAll(letra, "a")
-      } else if(letra==="2"){
-          stringOriginal= string.replaceAll(letra, "e")
-      } else if(letra==="3"){
-          stringOriginal= string.replaceAll(letra, "i")
-      } else if(letra==="4"){
-          stringOriginal= string.replaceAll(letra, "o")
-      } else if(letra==="5"){
-          stringOriginala= string.replaceAll(letra, "u")
-      }
-  }
-
-  return stringOriginal
-
+    
+const decode = (string) =>{
+    
+let letra1= string.replaceAll("1", "a");
+let letra2= letra1.replaceAll("2", "e");
+let letra3= letra2.replaceAll("3", "i");
+let letra4= letra3.replaceAll("4", "o");
+let letra5= letra4.replaceAll("5", "u");
+        
+        
+return letra5
+        
 }
 
 // Desafio 10 - Crie a função techList
