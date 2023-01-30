a19bbf3a89ce30638b1f3668d0e6a9a0aac6d10f
DelioAraujo <delioaraujo@gmail.com>
2023-01-30 14:59:06 -0300

    desafio 11 - tentativa 01

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,5 +1,36 @@
 // Desafio 11 - Crie a função generatePhoneNumber
 
+const generatePhojeNumber = (array)=>{
+    
+  let numeroFinal = `(${array[0]}${array[1]}) ${array[2]}${array[3]}${array[4]}${array[5]}${array[6]}-${array[7]}${array[8]}${array[9]}${array[10]}`
+
+  if(array.length!==11){
+      numeroFinal="Array com tamanho incorreto"
+  }
+
+  for(numero of array){
+      if(numero<0 || numero>9){
+          numeroFinal="não é possível gerar um número de telefone com esses valores"
+      }
+  }
+
+  let numeroRepetido=0
+
+  for(numero1 of array){
+      for(numero2 of array){
+          if (numero1===numero2){
+              numeroRepetido+=1
+          }
+      }
+  }
+
+  if(numeroRepetido>=3){
+      numeroFinal="não é possível gerar um número de telefone com esses valores"
+  }
+
+  return numeroFinal
+}
+
 // Desafio 12 -  Crie a função triangleCheck
 
 // Desafio 13 - Crie a função hydrate
