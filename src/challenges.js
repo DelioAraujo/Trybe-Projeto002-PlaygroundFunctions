eef6e6b6639673533cc944662c5f3a56d932d42d
DelioAraujo <delioaraujo@gmail.com>
2023-09-23 15:51:15 -0300

    sei la

--- src/challenges.js
+++ src/challenges.js
@@ -1,152 +1,140 @@
 // Desafio 1 - Crie a função compareTrue
 
-const compareTrue = (parametro1, parametro2)=>{
-  let resultado = ((parametro1&&parametro2)===true)? true : false
+const compareTrue = (parametro1, parametro2) => {
+  let resultado = ((parametro1 && parametro2) === true);
 
-  return resultado
-}
+  return resultado;
+};
 
 // Desafio 2 - Crie a função splitSentence
 
-const splitSentence = (string) => string.split(" ")
+const splitSentence = (string) => string.split(' ');
 
 // Desafio 3 - Crie a função concatName
 
-const concatName = (array) => `${array[array.length-1]}, ${array[0]}`
+const concatName = (array) => `${array[array.length - 1]}, ${array[0]}`;
 
 // Desafio 4 - Crie a função footballPoints
 
-const footballPoints = (wins, ties) =>(3*wins)+ties
+const footballPoints = (wins, ties) => (3 * wins) + ties;
 
 // Desafio 5 - Crie a função highestCount
 
-const highestCount = (array) =>{
-  let maiorNumero=array[0];
-  for(let i=1; i<array.length; i+=1){
-      if(array[i]>maiorNumero){
-          maiorNumero=array[i]
-      }
+const highestCount = (array) => {
+  let maiorNumero = array[0];
+  for (let i = 1; i < array.length; i += 1) {
+    if (array[i] > maiorNumero) {
+      maiorNumero = array[i];
+    }
   }
 
-  console.log("maior número: "+maiorNumero)
+  console.log(`maior número: ${maiorNumero}`);
 
-  let vezesAparece=0
-  for(i=0; i<array.length; i+=1){
-      if(array[i]===maiorNumero){
-          vezesAparece=vezesAparece+1
-      }
+  let vezesAparece = 0;
+  for (i = 0; i < array.length; i += 1) {
+    if (array[i] === maiorNumero) {
+      vezesAparece += 1;
+    }
   }
 
   return vezesAparece;
-}
+};
 
 // Desafio 6 - Crie as funções calcTriangleArea, calcRectangleArea e calcAllAreas
 
-const calcTriangleArea = (base, height)=> (base*height)/2;
+const calcTriangleArea = (base, height) => (base * height) / 2;
 
-const calcRectangleArea = (base, height) => base*height;
+const calcRectangleArea = (base, height) => base * height;
 
-const calcAllAreas = (base, height, form)=>{
-    let resposta = ""
+const calcAllAreas = (base, height, form) => {
+  let resposta = '';
 
-    if(form==="triângulo"){
-        resposta = `O valor da área do triângulo é de: ${calcTriangleArea (base, height)}`
-    } else if (form==="retângulo"){
-        resposta = `O valor da área do retângulo é de: ${calcRectangleArea (base, height)}`
-    } else {
-        resposta =  "Não foi possível fazer o cálculo, insira uma forma geométrica válida"
-    }
+  if (form === 'triângulo') {
+    resposta = `O valor da área do triângulo é de: ${calcTriangleArea(base, height)}`;
+  } else if (form === 'retângulo') {
+    resposta = `O valor da área do retângulo é de: ${calcRectangleArea(base, height)}`;
+  } else {
+    resposta = 'Não foi possível fazer o cálculo, insira uma forma geométrica válida';
+  }
 
-    return resposta;
-
-}
-
+  return resposta;
+};
 
 // Desafio 7 - Crie a função catAndMouse
 
-const catAndMouse = (mouse, cat1, cat2)=>{
-  letDistanciaCat1= Math.abs(mouse-cat1);
-  letDistanciaCat2= Math.abs(mouse-cat2);
-  
-  let maisProximo; 
+const catAndMouse = (mouse, cat1, cat2) => {
+  letDistanciaCat1 = Math.abs(mouse - cat1);
+  letDistanciaCat2 = Math.abs(mouse - cat2);
 
-  if(letDistanciaCat1<letDistanciaCat2){
-      maisProximo="cat1"
-  } else if (letDistanciaCat2<letDistanciaCat1){
-      maisProximo="cat2"
-  } else{
-      maisProximo = "os gatos trombam e o rato foge"
+  let maisProximo;
+
+  if (letDistanciaCat1 < letDistanciaCat2) {
+    maisProximo = 'cat1';
+  } else if (letDistanciaCat2 < letDistanciaCat1) {
+    maisProximo = 'cat2';
+  } else {
+    maisProximo = 'os gatos trombam e o rato foge';
   }
 
   return maisProximo;
-
-}
+};
 
 // Desafio 8 - Crie a função fizzBuzz
 
-const fizzBuzz = (array) =>{
-  novoArray=[];
-  for(numero of array){
-      if(numero%3===0 && numero%5!==0){
-          novoArray.push("fizz")
-      } else if( numero%3!==0 && numero%5===0){
-          novoArray.push("buzz")
-      } else if(numero%3===0 && numero%5===0){
-          novoArray.push("fizzBuzz")
-      } else { novoArray.push("bug!")}
+const fizzBuzz = (array) => {
+  novoArray = [];
+  for (numero of array) {
+    if (numero % 3 === 0 && numero % 5 !== 0) {
+      novoArray.push('fizz');
+    } else if (numero % 3 !== 0 && numero % 5 === 0) {
+      novoArray.push('buzz');
+    } else if (numero % 3 === 0 && numero % 5 === 0) {
+      novoArray.push('fizzBuzz');
+    } else { novoArray.push('bug!'); }
   }
-  return novoArray
-}
+  return novoArray;
+};
 
 // Desafio 9 - Crie a função encode e a função decode
 
-const encode = (string) =>{
+const encode = (string) => {
+  let letraA = string.replaceAll('a', '1');
+  let letraE = letraA.replaceAll('e', '2');
+  let letraI = letraE.replaceAll('i', '3');
+  let letraO = letraI.replaceAll('o', '4');
+  let letraU = letraO.replaceAll('u', '5');
 
-let letraA= string.replaceAll("a", "1");
-let letraE= letraA.replaceAll("e", "2");
-let letraI= letraE.replaceAll("i", "3");
-let letraO= letraI.replaceAll("o", "4");
-let letraU= letraO.replaceAll("u", "5");
-    
-    
-return letraU
-    
-}
-    
-const decode = (string) =>{
-    
-let letra1= string.replaceAll("1", "a");
-let letra2= letra1.replaceAll("2", "e");
-let letra3= letra2.replaceAll("3", "i");
-let letra4= letra3.replaceAll("4", "o");
-let letra5= letra4.replaceAll("5", "u");
-        
-        
-return letra5
-        
-}
+  return letraU;
+};
+
+const decode = (string) => {
+  let letra1 = string.replaceAll('1', 'a');
+  let letra2 = letra1.replaceAll('2', 'e');
+  let letra3 = letra2.replaceAll('3', 'i');
+  let letra4 = letra3.replaceAll('4', 'o');
+  let letra5 = letra4.replaceAll('5', 'u');
+
+  return letra5;
+};
 
 // Desafio 10 - Crie a função techList
 
-const techList = (array, string)=>{
-  arrayOrdenado=array.sort()
-  let lista=[];
-  for(techs of arrayOrdenado){
-     let objeto = {
-      tech:techs,
-      name:string
-     }
-     lista.push(objeto);
-
+const techList = (array, string) => {
+  arrayOrdenado = array.sort();
+  let lista = [];
+  for (techs of arrayOrdenado) {
+    let objeto = {
+      tech: techs,
+      name: string,
+    };
+    lista.push(objeto);
   }
 
-  if(array===[]){
-      return []
-  } else {
-      return lista
+  if (array === []) {
+    return [];
   }
-
-}
+  return lista;
+};
 
 // Não modifique essas linhas
 module.exports = {
--- src/challenges2.js
+++ src/challenges2.js
@@ -1,77 +1,67 @@
 // Desafio 11 - Crie a função generatePhoneNumber
 
-const generatePhoneNumber = (array)=>{
-    
-  let numeroFinal = `(${array[0]}${array[1]}) ${array[2]}${array[3]}${array[4]}${array[5]}${array[6]}-${array[7]}${array[8]}${array[9]}${array[10]}`
+const generatePhoneNumber = (array) => {
+  let numeroFinal = `(${array[0]}${array[1]}) ${array[2]}${array[3]}${array[4]}${array[5]}${array[6]}-${array[7]}${array[8]}${array[9]}${array[10]}`;
 
-  for(let i=0; i<array.length; i+=1){
-      let contador = 0
-      for(let i2=0; i2<array.length; i2+=1){
-          if(array[i]===array[i2]){
-              contador+=1
-          }
-          if(contador>=3){
-              numeroFinal = "não é possível gerar um número de telefone com esses valores";
-              break;    
-          }
-
+  for (let i = 0; i < array.length; i += 1) {
+    let contador = 0;
+    for (let i2 = 0; i2 < array.length; i2 += 1) {
+      if (array[i] === array[i2]) {
+        contador += 1;
       }
-  }
-
-  for(numero of array){
-      if(numero<0 || numero>9){
-          numeroFinal="não é possível gerar um número de telefone com esses valores"
+      if (contador >= 3) {
+        numeroFinal = 'não é possível gerar um número de telefone com esses valores';
+        break;
       }
+    }
   }
 
-  if(array.length!==11){
-      numeroFinal="Array com tamanho incorreto."
+  for (numero of array) {
+    if (numero < 0 || numero > 9) {
+      numeroFinal = 'não é possível gerar um número de telefone com esses valores';
+    }
   }
 
- 
-  
-  
+  if (array.length !== 11) {
+    numeroFinal = 'Array com tamanho incorreto.';
+  }
+
   return numeroFinal;
-}
+};
 
 // Desafio 12 -  Crie a função triangleCheck
 
-const triangleCheck = (lineA, lineB, lineC) =>{
-  //teste 1
-  let testA1 = lineA<lineB+lineC
-  let testB1 = lineB<lineA+lineC
-  let testC1 = lineC<lineB+lineA
-  let teste1 = (testA1 ===true && testB1===true && testC1===true)? true : false;
-  //teste 2
-  let testA2 = lineA>Math.abs(lineB-lineC)
-  let testB2 = lineB>Math.abs(lineA-lineC)
-  let testC2 = lineC>Math.abs(lineB-lineA)
-  let teste2 = (testA2 ===true && testB2===true && testC2===true)? true : false;
-  //resultado
-  let resultado = (teste1===true && teste2===true)? true : false;
+const triangleCheck = (lineA, lineB, lineC) => {
+  // teste 1
+  let testA1 = lineA < lineB + lineC;
+  let testB1 = lineB < lineA + lineC;
+  let testC1 = lineC < lineB + lineA;
+  let teste1 = !!((testA1 === true && testB1 === true && testC1 === true));
+  // teste 2
+  let testA2 = lineA > Math.abs(lineB - lineC);
+  let testB2 = lineB > Math.abs(lineA - lineC);
+  let testC2 = lineC > Math.abs(lineB - lineA);
+  let teste2 = !!((testA2 === true && testB2 === true && testC2 === true));
+  // resultado
+  let resultado = !!((teste1 === true && teste2 === true));
 
-  return resultado
-
-}
+  return resultado;
+};
 
 // Desafio 13 - Crie a função hydrate
 
-const hydrate = (string)=>{
-
-    
+const hydrate = (string) => {
   let arrayDasStringsNumericas = string.match(/\d+/g);
-  let numeroDeDoses=0
+  let numeroDeDoses = 0;
 
-  for(numeros of arrayDasStringsNumericas){
-      numeroDeDoses+=+numeros 
+  for (numeros of arrayDasStringsNumericas) {
+    numeroDeDoses += +numeros;
   }
 
-  let conselho = (numeroDeDoses===1)? `${numeroDeDoses} copo de água` : `${numeroDeDoses} copos de água` 
- 
-  return conselho
+  let conselho = (numeroDeDoses === 1) ? `${numeroDeDoses} copo de água` : `${numeroDeDoses} copos de água`;
 
-
-}
+  return conselho;
+};
 
 /* eslint no-undef: 0 */
 
